8d3efe5f7a68d84d3bb2f7b2c67c7a51e80cdbdb
Marvirydian <31254602+Marvirydian@users.noreply.github.com>
2017-10-20 12:50:14 -0500

    Add files via upload

---
+++ clase20.cc
@@ -0,0 +1,19 @@
+//Programa que al darle un número entero cualquiera da como resultado  el factorial del mismo
+//Así para distintos valores de números nos mostrará distintos resultados
+//Marlene Guerra Santiago
+//Clase del 20 de Octubre de 2017
+
+#include <iostream>
+ using namespace std;
+int main ()
+{
+  int N=0; //inicia el contador desde cero
+  cout <<"Dame un número entero" << endl; //el programa requiere algún número para iniciar a trabajar
+cin >> N;
+ int M=1; // M es el contador debe ser igual a N
+ int F=1; // Servirá para multiplicar el número inicial y el siguiente generando uno nuevo, toma la forma del factorial
+while (M!=N)
+if {M=M+1; F=F*M; }
+ cout << "Valor final" <<F<<endl; //Aqui es donde realiza el ciclo y contiene el contador para detener el programa, cuando termina muestra el factorial del número inicial N
+ return 0;
+}
